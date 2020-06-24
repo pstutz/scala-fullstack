4a75518f01cb15d3d32d4b40f5f0fd3d4bfc7af5
Philip Stutz <philip.stutz@gmail.com>
2020-06-24 15:13:43 +0200

    Disable RPC logging

--- frontend/src/webapp/RPC.scala
+++ frontend/src/webapp/RPC.scala
@@ -1,14 +1,19 @@
 package webapp
 
 import java.nio.ByteBuffer
+
 import boopickle.Default._
 import chameleon.ext.boopickle._
 import covenant.http.HttpClient
-import sloth.{Client, ClientException}
+import sloth.{Client, ClientException, LogHandler}
+
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
 object RPC {
-  private val client: Client[ByteBuffer, Future, ClientException] = HttpClient[ByteBuffer]("/api")
+  private val client: Client[ByteBuffer, Future, ClientException] = HttpClient[ByteBuffer](
+    "/api",
+    LogHandler.empty[Future] // Remove to enable default logger
+  )
   val exampleApi: ExampleApi = client.wire[ExampleApi]
 }
