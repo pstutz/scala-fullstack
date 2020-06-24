33ab963b46706da88454b60efaae8e70e180a82a
Philip Stutz <philip.stutz@gmail.com>
2020-06-24 15:03:46 +0200

    Improve HTTP server config

--- backend/src/webapp/WebServer.scala
+++ backend/src/webapp/WebServer.scala
@@ -1,37 +1,31 @@
 package webapp
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.settings.{Http2ServerSettings, ServerSettings}
-import akka.http.scaladsl.{Http, HttpConnectionContext}
 import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 
 object WebServer {
-  val enableHttp2 = "akka.http.server.preview.enable-http2 = on"
 
   def main(args: Array[String]): Unit = {
-    val config: Config = ConfigFactory
-      .parseString(enableHttp2)
-      .withFallback(ConfigFactory.load())
+    val config: Config = ConfigFactory.load()
 
     implicit val system: ActorSystem = ActorSystem("server-system", config)
     implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-    val interface = config.getString("http.interface")
-    val port = config.getInt("http.port")
     val serverFuture: Future[Http.ServerBinding] = Http().bindAndHandleAsync(
       handler = Route.asyncHandler(new Router().routes),
-      interface = interface,
-      port = port,
-      connectionContext = HttpConnectionContext(),
-      settings = ServerSettings(config).withHttp2Settings(Http2ServerSettings(config)),
-      parallelism = Runtime.getRuntime.availableProcessors()
+      interface = "localhost",
+      settings = ServerSettings(config).withHttp2Settings(Http2ServerSettings(config))
     )
-    Await.result(serverFuture, Duration.Inf)
+    val server = Await.result(serverFuture, Duration.Inf)
+    val host = server.localAddress.getAddress.getHostName
+    val port = server.localAddress.getPort
 
-    println(s"Server online at http://$interface:$port")
+    println(s"Server online at http://$host:$port")
   }
 }
